4461dd6e8a076dca97dcd208ea0e18d9fa33c886
Elias Manuel Nuñez <tzanu007@gmail.com>
2025-12-14 13:55:13 -0300

    18 elias, y 17 arron

--- Practico-5-Arreglos/e17.java
+++ Practico-5-Arreglos/e17.java
@@ -1,6 +1,28 @@
 public class e17{
+   final static int MAX=10;
     public static void main(String[] args) {
-        
+        int []arr={0,1,2,3,4,5,6,7,8,9};
+        System.out.println("ingrese un numero: ");
+        int pos=Utils.leerInt();
+        System.out.println();
+        mostrar(arr);
+        meterN(arr,pos);
+        System.out.println();
+        mostrar(arr);
+    }
+    public static void meterN(int[]arr, int n){
+        int fin=MAX-1;
+        int ini=0;
+        while(ini<fin){
+            arr[fin]=arr[fin-1];
+            fin--;
+        }
+        arr[ini]=n;
+    }
+    public static void mostrar(int[] a){
+        for(int i=0;i<MAX;i++){
+            System.out.print("|"+a[i] +"| ");
+        }
     }
 }
 /*Hacer un programa que dado un arreglo de enteros de tamaño
--- Practico-5-Arreglos/ejer18Elias.java
+++ Practico-5-Arreglos/ejer18Elias.java
@@ -1,6 +1,45 @@
 public class ejer18Elias {
+  final static int MAX = 10;
+
   public static void main(String[] args) {
-    
+    int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int numero = pedir_numero();
+    System.out.println(numero);
+    mostrar_arr(arr);
+    buscar_numero(arr, numero);
+    System.out.println("El numero es: " +numero+ "asi queda el arreglo despues de eliminarlo: ");
+    mostrar_arr(arr);
+  }
+
+  public static void buscar_numero(int[] arr, int numero) {
+    int pos = 0;
+    while (pos < MAX) {
+      if (arr[pos] == numero) {
+        corrimiento_izquierda(arr,pos);
+      }
+      pos++;
+    }
+  }
+  
+  public static void corrimiento_izquierda(int[] arr, int pos) {
+    int posicion = arr[pos];
+    while (posicion<MAX-1) {
+      arr[posicion] = arr[posicion + 1];
+      posicion++;
+    }
+  }
+
+  public static void mostrar_arr(int[] arr) {
+    int pos = 0;
+    while (pos < MAX) {
+      System.out.print(arr[pos] + " | ");
+      pos++;
+    }System.out.println();
+  }
+  
+  public static int pedir_numero() {
+    System.out.println("Ingrese un numero para eliminarlo del arreglo");
+    return Utils.leerInt();
   }
 }
 /*
@@ -10,7 +49,8 @@ public class ejer18Elias {
 entero  y  elimine  la  primera  ocurrencia  del  número  (un  número 
 igual)  en  el  arreglo  (si  existe).  Para  ello  tendrá  que  buscar  la 
 posición  y  si  está,  realizar  un  corrimiento  a  izquierda  (queda  una 
-copia de la última posición del arreglo en la anteúltima posición). 
+copia de la última posición del arreglo en la anteúltima posición).
+
 19.  Hacer  un  programa  que  dado  un  arreglo  de  enteros  de  tamaño 
 10  que  se  encuentra  precargado,  solicite  al  usuario  un  número 
 entero  y  elimine  todas  las  ocurrencia  de  número  en  el  arreglo. 
